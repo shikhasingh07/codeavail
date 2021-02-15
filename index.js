de311ebed5f0735c5d2e79af3e132c424ef38621
--global <--global>
2021-02-15 22:53:53 +0530

    include ejs

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const port = 8000;
 // use express router
 app.use('/', require('./routes'));
 
+// FOR Setting  eJS part 
+app.set('view engine', 'ejs');
+app.set('view', './view');
+
 app.listen(port, function(err){
     if (err){
         console.log(`Error in running the server: ${err}`);
